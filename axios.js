e42ada9312afab6ab50151e93baeda32483d7776
Divij Kardile <31166128+divijkardile@users.noreply.github.com>
2020-11-16 00:13:18 +0530

    Delete axios.js

--- axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:5001/studentbridge-24ac7/us-central1/api' //API (cloud function) URL
-});
-
-export default instance;
